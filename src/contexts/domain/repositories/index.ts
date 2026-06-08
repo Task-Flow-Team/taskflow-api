f53e3ad39f922c2736d85f8ee5b0f0ecb810d2d5
Klout. <55015851+KloutDevs@users.noreply.github.com>
2026-06-07 19:17:46 -0600

    fix: update barrel to export from workspace.repository.port (typo fix)

--- src/contexts/domain/repositories/index.ts
+++ src/contexts/domain/repositories/index.ts
@@ -2,4 +2,4 @@ export * from './subtask.repository.port';
 export * from './tag.repository.port';
 export * from './task.repository.port';
 export * from './user.repository.port';
-export * from './workspsace.repository.port';
+export * from './workspace.repository.port';
